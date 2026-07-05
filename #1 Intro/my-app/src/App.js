cd4695504f9efefb2721cdd07f6a89b77ad18d77
Ahmadrazasial <ahmadrazauch15@gmail.com>
2026-07-05 16:42:03 +0500

    React Started with Intro

---
+++ #1 Intro/my-app/src/App.js
@@ -0,0 +1,61 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+import React from 'react';
+
+function App() {
+
+  const [count , setCount] = useState(0);
+  function increment() {
+    setCount(count + 1);
+  }
+
+  return (
+    <div className="App">
+     <div className="count"> {count} </div>
+     <button onClick={increment}>Increment</button>
+    </div>
+  );
+}function App() {
+
+  const [count , setCount] = useState(0);
+  function increment() {
+    setCount(count + 1);
+  }
+
+  return (
+    <div className="App">
+     <div className="count"> {count} </div>
+     <button onClick={increment}>Increment</button>
+    </div>
+  );
+}function App() {
+
+  const [count , setCount] = useState(0);
+  function increment() {
+    setCount(count + 1);
+  }
+
+  return (
+    <div className="App">
+     <div className="count"> {count} </div>
+     <button onClick={increment}>Increment</button>
+    </div>
+  );
+}
+function App() {
+
+  const [count , setCount] = useState(0);
+  function increment() {
+    setCount(count + 1);
+  }
+
+  return (
+    <div className="App">
+     <div className="count"> {count} </div>
+     <button onClick={increment}>Increment</button>
+    </div>
+  );
+}
+
+export default App;
