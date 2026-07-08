d7987d341efc207533ccc8e44b025e963459270a
Ahmadrazasial <ahmadrazauch15@gmail.com>
2026-07-08 23:42:21 +0500

    Tried components

--- #1 Intro/my-app/src/App.js
+++ #1 Intro/my-app/src/App.js
@@ -4,33 +4,6 @@ import { useState } from 'react';
 import React from 'react';
 
 function App() {
-
-  const [count , setCount] = useState(0);
-  function increment() {
-    setCount(count + 1);
-  }
-
-  return (
-    <div className="App">
-     <div className="count"> {count} </div>
-     <button onClick={increment}>Increment</button>
-    </div>
-  );
-}function App() {
-
-  const [count , setCount] = useState(0);
-  function increment() {
-    setCount(count + 1);
-  }
-
-  return (
-    <div className="App">
-     <div className="count"> {count} </div>
-     <button onClick={increment}>Increment</button>
-    </div>
-  );
-}function App() {
-
   const [count , setCount] = useState(0);
   function increment() {
     setCount(count + 1);
@@ -43,19 +16,8 @@ function App() {
     </div>
   );
 }
-function App() {
-
-  const [count , setCount] = useState(0);
-  function increment() {
-    setCount(count + 1);
-  }
-
-  return (
-    <div className="App">
-     <div className="count"> {count} </div>
-     <button onClick={increment}>Increment</button>
-    </div>
-  );
-}
-
 export default App;
+
+
+
+
---
+++ First-app/First-React-app/src/components/footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './footer.css'
+
+const Footer = () => {
+  return (
+    <div>
+    <footer className="footer">
+      
+       <div><p>&copy; 2023 My App. All rights reserved.</p></div> 
+      
+    </footer>
+    </div>
+  )
+}
+
+export default Footer
