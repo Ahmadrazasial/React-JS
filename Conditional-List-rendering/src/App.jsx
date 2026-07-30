9013249c2beb74e62480bf81176a5065ba8b5c8d
Ahmadrazasial <ahmadrazauch15@gmail.com>
2026-07-30 23:58:35 +0500

    started list rendering

--- Conditional-List-rendering/src/App.jsx
+++ Conditional-List-rendering/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState,createContext,useContext } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
@@ -9,34 +9,63 @@ import List from './condition'
 function App() {
   const [count, setCount] = useState(0)
   const [btn, setBtn] = useState(false);
-function ShowBtn() {
-  //Using if/else statement
-  // if(!btn){
-  //   return(
-  //   <>
-  //   <button>I am a false btn</button>
-  //    </> )
-  // }
-  //   return <button>I am a true btn</button>
 
-  // using ternary operator
-  // return (
-  //   btn ? <button>i am a true btn 0</button> : <button>i am a false btn 0</button>
-  // )
+  //Conditional Rendering
+  function ShowBtn() {
+    // Using if/else statement 
+    // if(!btn){
+    //   return(
+    //   <>
+    //   <button>I am a false btn</button>
+    //    </> )
+    // }
+    //   return <button>I am a true btn</button>
 
-  //Simply ternary
-  // return btn ?  <button>i am a true btn</button> :  <button>i am a false btn</button>
-     
+    // using ternary operator
+    // return (
+    //   btn ? <button>i am a true btn 0</button> : <button>i am a false btn 0</button>
+    // )
 
-  //Using Ampersand
+    //Simply ternary
+    // return btn ?  <button>i am a true btn</button> :  <button>i am a false btn</button>
 
-  // return btn && <button>This is only true btn</button>
 
-  if(btn){
-    return null
+    //Using Ampersand
+
+    // return btn && <button>This is only true btn</button>
+
+    if (btn) {
+      return null
+    }
+    return <button>This is only true btn</button>
   }
-  return <button>This is only true btn</button>
-}
+
+  //List rendering
+  const [lists,setLists] = useState([
+    {
+      id:0,
+      title:"Web devleopment"
+    },
+    {
+      id:1,
+      title:"App devleopment"
+    },
+    {
+      id:2,
+      title:"Software devleopment"
+    }
+  ])
+ 
+  function Item({list}) {
+    
+    return btn ? (
+    <div className="border border-purple-400 m-4 px-2 py-1 border-2 "> 
+    <li>{list.title}</li>
+    </div>
+    ):null;
+  }
+
+
   return (
     <>
       <section id="center">
@@ -51,8 +80,20 @@ function ShowBtn() {
             Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
           </p>
         </div>
-<List/>
-        <ShowBtn/>
+        <List />
+        <ShowBtn />
+            <ul className={`overflow-hidden transition-all duration-500  ${
+              btn ? "translate-y-0 opacity-100":"translate-y-5 opacity-0" 
+            }`}>
+              <h3 className='text-blue-600 font-bold tran'>List of courses</h3>
+              {lists.map(list =>{
+                return(
+              <div key={list.id}>
+              {<Item  list={list}/>}
+              </div>
+                )
+              })}
+            </ul>
         <button
           type="button"
           className="counter"
