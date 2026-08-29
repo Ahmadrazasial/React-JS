b447afa5fdb030d02169f6df60326cd39ec08b34
Ahmadrazasial <ahmadrazauch15@gmail.com>
2026-08-29 21:12:53 +0500

    did few examples event handler functions passing as props to tags

--- Handling-Events/src/App.jsx
+++ Handling-Events/src/App.jsx
@@ -1,121 +1,49 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import heroImg from './assets/hero.png'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
-import './App.css'
+import './app.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [name, setName] = useState("Ahmad");
+  const [form, setForm] = useState({ email: " ", phone: " " });
 
+  const handleClick = () => {
+    alert("I am clicked")
+  }
+  const handleOnHover = () => {
+    alert("I was hovered")
+  }
+
+  const handleChange = (e) => {
+    setName(e.target.value)
+    console.log(name)
+
+  }
+  useEffect(() => {
+    console.log(name)
+  }, [name])
+  const valueChange = (e) => {
+    // e.target.name === "email" ? setForm({...form,email:e.target.value}):setForm({...form,phone:e.target.value})
+    setForm({...form, [e.target.name]: e.target.value})
+    console.log(form)
+  }
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+      <div className="button">
+        <button onClick={handleClick}>Click me</button>
+      </div>
+      <div className="red" onMouseOver={handleOnHover} >
+        I am a red div
+      </div>
+      <input type="text" className="name w-[20%] border rounded px-3 py-1" value={name} onChange={handleChange} />
 
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+      <div className="inputs">
+        <input type="text" name="email" value={form.email} onChange={valueChange} className="email w-[20%] border rounded px-3 py-1" />
+        <input type="text" name='phone' value={form.phone} onChange={valueChange} className="phone w-[20%] border rounded px-3 py-1" />
+      </div>
     </>
+
   )
 }
 
